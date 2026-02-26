c88d45c886c552aba6f29a81637f06d026db1eda
Michael <60126397+michaelmikeee@users.noreply.github.com>
2026-02-26 22:14:30 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app  = express();
+
+const port  = process.env.PORT || 8000
+
+app.use(express.json());
+
+app.get('/', function (req, res) {
+    res.send('Hello From Express Server!');
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
